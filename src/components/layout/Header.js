a71040b2b95288a464d1aebaeecc33441e292177
Sean Modd <seanmodd@Seans-MacBook-Pro.local>
2020-12-11 13:53:34 -0800

    not correctly showing dark mode and light mode for some reason...

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,25 +1,52 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
-
-const menuData = [
-  { title: "Courses", icon: "/images/icons/courses.svg", link: "/courses" },
-  {
-    title: "Tutorials",
-    icon: "/images/icons/tutorials.svg",
-    link: "/tutorials",
-  },
-  { title: "Pricing", icon: "/images/icons/pricing.svg", link: "/pricing" },
-]
+import styled from "styled-components"
+import { menuData } from "../../data/menuData"
+import MenuButton from "../buttons/MenuButton"
+import MenuTooltip from "../tooltips/MenuTooltip"
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false)
+  function handleClick(event) {
+    setIsOpen(!isOpen)
+    event.preventDefault()
+    console.log(event)
+  }
   return (
-    <>
-      {menuData.map((item, index) => (
-        <Link to={item.link} key={index}>
-          <img src={item.icon} alt={item.title} />
-          {item.title}
-        </Link>
-      ))}
-    </>
+    <Wrapper>
+      <Link to="/">
+        <img src="/images/logos/logo.svg" alt="Logo" />
+      </Link>
+      <MenuWrapper count={menuData.length}>
+        {menuData.map((item, index) =>
+          item.link === "/account" ? (
+            <MenuButton
+              item={item}
+              key={index}
+              onClick={event => handleClick(event)}
+            />
+          ) : (
+            <MenuButton item={item} key={index} />
+          )
+        )}
+      </MenuWrapper>
+      <MenuTooltip isOpen={isOpen} />
+    </Wrapper>
   )
 }
+const Wrapper = styled.div`
+  position: absolute;
+  top: 60px;
+  display: grid;
+  grid-template-columns: 44px auto;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 30px;
+`
+
+const MenuWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(${props => props.count}, auto);
+  gap: 30px;
+`
--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,17 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
 import { H1, MediumText } from "../styles/TextStyles"
 import { themes } from "../styles/ColorStyles"
 import PurchaseButton from "../buttons/PurchaseButtons"
 import MockupAnimation from "../animations/MockupAnimation"
+import WaveBackground from "../backgrounds/WaveBackground"
 
 function HeroSection() {
   return (
     <Wrapper>
+      <WaveBackground />
       <ContentWrapper>
         <TextWrapper>
-          <Title>Hi all. Love you people.</Title>
+          <Title>
+            Hi all. <span>Love you people.</span>
+          </Title>
           <Description>Welcome to your new Gatsby site.</Description>
           <PurchaseButton
             title="Start learning"
@@ -26,23 +30,90 @@ function HeroSection() {
 
 export default HeroSection
 
-const Wrapper = styled.div`
-  background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
-  overflow: hidden;
+const animation = keyframes`
+  0% { opacity: 0; transform: translateY(-10px); filter: blur(10px); }
+  80% { opacity: 0.5; transform: translateY(-10px); filter: blur(10px); }
+  100% { opacity: 1; transform: translateY(0px); filter: blur(0px); }
 `
+
+const Wrapper = styled.div`
+  position: absolute;
+  top: 60px;
+  display: grid;
+  grid-template-columns: 44px, auto;
+  width: 100%;
+  justify-content: space-between;
+  padding: 0 30px;
+  align-items: center;
+  @media (max-width: 768px) {
+    top: 30px;
+  }
+  @media (max-width: 450px) {
+    top: 20px;
+    padding: 0 20px;
+  }
+`
+const MenuWrapper = styled.div`
+  display: grid;
+  gap: 30px;
+  grid-template-columns: repeat() (${props => props.count}, auto);
+
+  @media (max-width: 768px) {
+    > a {
+      display: none;
+    }
+    grid-template-columns: auto;
+  }
+`
+const HamburgerWrapper = styled.div`
+  display: none;
+  @media (max-width: 768px) {
+    display: block;
+  }
+`
+
 const ContentWrapper = styled.div`
   max-width: 1234px;
   margin: 0 auto;
   padding: 200px 30px;
   display: grid;
   grid-template-columns: 360px auto;
+  @media (max-width: 450px) {
+    grid-template-columns: auto;
+    padding: 150px 20px 250px;
+    gap: 60px;
+  }
 `
 const TextWrapper = styled.div`
   max-width: 360px;
   display: grid;
   gap: 30px;
+  > * {
+    :nth-child(1) {
+      animation: ${animation} 1s 0s forwards;
+    }
+    :nth-child(2) {
+      animation: ${animation} 1s 0.4s forwards;
+    }
+    :nth-child(3) {
+      animation: ${animation} 1s 0.6s forwards;
+    }
+  }
 `
 const Title = styled(H1)`
   color: ${themes.dark.text1};
+  background: linear-gradient(180deg, #730040 0%, #301cbe 100%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+  span {
+    background: linear-gradient(180deg, #ffd7ff 0%, #ffb6ff 100%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    color: transparent;
+  }
+  @media (max-width: 450px) {
+    font-size: 48px;
+  }
 `
 const Description = styled(MediumText)``
